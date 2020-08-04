45c9e6c4d140426c9c2b4e96b6b245e20683f512
vn <vn@vns-MacBook-Pro.local>
2020-08-04 21:35:23 +1200

    Task2Done

--- src/TaskTwo_StylePackages/CSSModules.js
+++ src/TaskTwo_StylePackages/CSSModules.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import classes from './styles.css'
 
 const CSSModules = () => {
+    const assignedClasses = [];
+    assignedClasses.push(classes.divStyle);
     return (
-        <div>
+        <div className={assignedClasses.join(' ')}>
             This is a CSS Moduale
         </div>
     );
